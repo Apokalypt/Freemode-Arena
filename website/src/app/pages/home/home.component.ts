0b97984d58de4e2a819513cdd39da8a1ed4b2c3f
Apokalypt <58733915+Apokalypt@users.noreply.github.com>
2023-11-10 13:41:01 +0100

    feat(Home): Draft design and ideas for interface

--- website/src/app/pages/home/home.component.ts
+++ website/src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 import { CommonModule } from '@angular/common';
 import { HeaderPaddingComponent } from "../../core/components/header-padding/header-padding.component";
 
 @Component({
     selector: 'fa-home',
     standalone: true,
-    imports: [CommonModule, HeaderPaddingComponent],
+    imports: [CommonModule, HeaderPaddingComponent, RouterLink],
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
 })
